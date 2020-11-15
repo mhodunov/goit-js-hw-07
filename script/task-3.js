ccb7eb670096c846ef853665755fdf72f30d5022
Maksym Hodunov <m.godunov@yandex.ru>
2020-11-15 19:00:06 +0200

    Task 3 complete

---
+++ script/task-3.js
@@ -0,0 +1,32 @@
+const images = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const gallery = document.querySelector('#gallery');
+const galleryItems = [];
+
+images.forEach(image => {
+  const galleryElement = document.createElement('li');
+  const galleryImage = document.createElement('img');
+  galleryImage.src = image.url;
+  galleryImage.alt = image.alt;
+  galleryElement.appendChild(galleryImage);
+  galleryElement.classList.add('gallery-item');
+  galleryItems.push(galleryElement);
+});
+
+gallery.append(...galleryItems);
